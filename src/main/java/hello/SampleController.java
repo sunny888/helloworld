1050e3c247e5b95ab8fbbdcdeab5a6d46c41435f
hzm_study@sina.com <hzm_study@sina.com>
2018-04-26 18:31:40 +0800

    11

---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+    	 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+         //System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+        return "Openshift Hello World!"+df.format(new Date());
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
