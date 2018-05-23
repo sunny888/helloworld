ed340fcd14108100fb5637a076f84e31572afd21
hzm_study@sina.com <hzm_study@sina.com>
2018-05-23 14:42:12 +0800

    ++

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
+	int num = (int) (Math.random() * 1000);
     @RequestMapping("/")
     @ResponseBody
     String home() {
     	 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
          //System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
-        return "Openshift Hello World!"+df.format(new Date());
+        return "MyID "+num+" Openshift Hello World!"+df.format(new Date());
     }
 
     public static void main(String[] args) throws Exception {
